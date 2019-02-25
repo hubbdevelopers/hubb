22179298a639c4a35b0079e3b7c98d7624eb9560
齊藤 信臣 (Saito Nobutaka) <saito.n@wingarc.com>
2019-02-25 22:17:16 +0900

    API に認証を設ける．

--- controllers/baseController.go
+++ controllers/baseController.go
@@ -1,8 +1,18 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	firebase "firebase.google.com/go"
+	"github.com/gin-gonic/gin"
 	"github.com/hubbdevelopers/hubb/db"
 	"github.com/jinzhu/gorm"
+	"google.golang.org/api/option"
 )
 
 var orm *gorm.DB
@@ -10,4 +20,38 @@ var orm *gorm.DB
 func Init() {
 	orm = db.GetORM()
 	//orm.LogMode(true)
+}
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+		if idToken == "" {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		opt := option.WithCredentialsFile(os.Getenv("SECRETS_FILE"))
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			log.Printf("error initializing app: %v\n", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		client, err := app.Auth(context.Background())
+		if err != nil {
+			log.Printf("error getting Auth client: %v\n", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		token, err := client.VerifyIDToken(c, idToken)
+		if err != nil {
+			log.Printf("error verifying ID token: %v\n", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+		log.Println(token.UID)
+		fmt.Println("Authorized!!")
+		c.Next()
+	}
 }
--- main/hubb.go
+++ main/hubb.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-	"reflect"
-	"strings"
 	"time"
 
-	firebase "firebase.google.com/go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/hubbdevelopers/hubb/controllers"
 	"github.com/hubbdevelopers/hubb/db"
 	"github.com/jinzhu/gorm"
-	"google.golang.org/api/option"
 )
 
 type Product struct {
@@ -33,9 +25,12 @@ func main() {
 
 	controllers.Init()
 
-	r := gin.Default()
+	r := setupRouter()
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
 
-	r.Use(auth())
+func setupRouter() *gin.Engine {
+	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
@@ -49,80 +44,47 @@ func main() {
 		MaxAge: 12 * time.Hour,
 	}))
 
+	// TODO この API に認証をかけると Login と Signup ができない
 	r.GET("/users", controllers.GetUsers)
-	r.GET("/users/:id", controllers.GetUser)
-	r.POST("/users", controllers.CreateUser)
-	r.PUT("/users/:id/init", controllers.InitializeUser)
-	r.PUT("/users/:id/images", controllers.UpdateImage)
-	r.PUT("/users/:id/profile", controllers.UpdateProfile)
 
-	r.GET("/users/:id/timeline", controllers.GetTimeline)
+	authenticated := r.Group("/")
+	authenticated.Use(controllers.Authenticate())
+	{
+		authenticated.GET("/users/:id", controllers.GetUser)
+		authenticated.POST("/users", controllers.CreateUser)
+		authenticated.PUT("/users/:id/init", controllers.InitializeUser)
+		authenticated.PUT("/users/:id/images", controllers.UpdateImage)
+		authenticated.PUT("/users/:id/profile", controllers.UpdateProfile)
 
-	r.GET("/users/:id/followings", controllers.GetUserFollowings)
-	r.GET("/users/:id/followers", controllers.GetUserFollowers)
-	//r.GET("/communities/:id/followers", controllers.GetCommunityFollowers)
-	r.POST("/users/:id/follow/users/:followingid", controllers.FollowUser)
-	r.DELETE("/users/:id/follow/users/:followingid", controllers.UnfollowUser)
-	r.POST("/users/:id/follow/communities/:followingid", controllers.FollowCommunity)
-	r.DELETE("/users/:id/follow/communities/:followingid", controllers.UnfollowCommunity)
+		authenticated.GET("/users/:id/timeline", controllers.GetTimeline)
 
-	r.GET("/pages", controllers.GetPages)
-	r.GET("/pages/:id", controllers.GetPage)
-	r.POST("/pages", controllers.CreatePage)
-	r.DELETE("/pages/:id", controllers.DeletePage)
-	r.PUT("/pages/:id", controllers.UpdatePage)
+		authenticated.GET("/users/:id/followings", controllers.GetUserFollowings)
+		authenticated.GET("/users/:id/followers", controllers.GetUserFollowers)
+		//authenticated.GET("/communities/:id/followers", controllers.GetCommunityFollowers)
+		authenticated.POST("/users/:id/follow/users/:followingid", controllers.FollowUser)
+		authenticated.DELETE("/users/:id/follow/users/:followingid", controllers.UnfollowUser)
+		authenticated.POST("/users/:id/follow/communities/:followingid", controllers.FollowCommunity)
+		authenticated.DELETE("/users/:id/follow/communities/:followingid", controllers.UnfollowCommunity)
 
-	r.GET("/comments", controllers.GetComments)
-	r.POST("/comments", controllers.CreateComment)
-	r.DELETE("/comments/:id", controllers.DeleteComment)
+		authenticated.GET("/pages", controllers.GetPages)
+		authenticated.GET("/pages/:id", controllers.GetPage)
+		authenticated.POST("/pages", controllers.CreatePage)
+		authenticated.DELETE("/pages/:id", controllers.DeletePage)
+		authenticated.PUT("/pages/:id", controllers.UpdatePage)
 
-	r.GET("/likes", controllers.GetLikes)
-	r.POST("/likes", controllers.CreateLike)
-	r.DELETE("/likes", controllers.DeleteLike)
+		authenticated.GET("/comments", controllers.GetComments)
+		authenticated.POST("/comments", controllers.CreateComment)
+		authenticated.DELETE("/comments/:id", controllers.DeleteComment)
 
-	r.GET("/communities", controllers.GetCommunities)
-	r.GET("/communities/:id", controllers.GetCommunity)
-	r.POST("/communities", controllers.CreateCommunity)
-	r.DELETE("/communities/:id", controllers.DeleteCommunity)
+		authenticated.GET("/likes", controllers.GetLikes)
+		authenticated.POST("/likes", controllers.CreateLike)
+		authenticated.DELETE("/likes", controllers.DeleteLike)
 
-	r.Run() // listen and serve on 0.0.0.0:8080
-}
-
-func auth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		opt := option.WithCredentialsFile(os.Getenv("SECRETS_FILE"))
-		app, err := firebase.NewApp(context.Background(), nil, opt)
-		if err != nil {
-			log.Fatalf("error initializing app: %v\n", err)
-		}
-
-		client, err := app.Auth(context.Background())
-		if err != nil {
-			log.Fatalf("error getting Auth client: %v\n", err)
-		}
-
-		fmt.Println(reflect.TypeOf(client))
-
-		authHeader := c.GetHeader("Authorization")
-		// fmt.Println(authHeader)
-
-		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
-		// fmt.Println(idToken)
-
-		if idToken != "" {
-			token, err := client.VerifyIDToken(c, idToken)
-			if err != nil {
-				log.Fatalf("error verifying ID token: %v\n", err)
-				c.Next()
-			}
-
-			log.Println(token.UID)
-			fmt.Println("Authorized!!")
-
-			// TODO
-		}
-
-		c.Next()
+		authenticated.GET("/communities", controllers.GetCommunities)
+		authenticated.GET("/communities/:id", controllers.GetCommunity)
+		authenticated.POST("/communities", controllers.CreateCommunity)
+		authenticated.DELETE("/communities/:id", controllers.DeleteCommunity)
 	}
+
+	return r
 }
